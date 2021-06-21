5467ea15dcecc9860b8bfe3312e976a3f4572f95
Janaki <janakishreeja23@gmail.com>
2021-06-21 17:22:44 +0400

    chors - style asset loader setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import App from "./App";
-
+import Logo from "./logo.svg";
 const root = document.getElementById("root");
 
 root.innerHTML = `
 <h1> learning web tooling today </h1>
-${App({ name: "Tanay" })}
+<img src="${Logo}"/>
+${App({ name: "Janaki" })}
 `;
--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,14 @@ const webpackConfig = {
           },
         },
       },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
     ],
   },
   mode: "production",
